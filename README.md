576b380f257979719db1d35a331685d15709a97a
steve jordan <nongninglele2@gmail.com>
2025-03-26 03:34:21 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BiblioTrack
-Application desktop ( JavaFX ) de suivi et gestion efficaces des livres,  emprunts et retours.
+
+Application desktop ( JavaFX ) de suivi et gestion efficaces des livres, emprunts et retours.
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.bibliotrack.controller {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.bibliotrack.controller to javafx.fxml;
+    exports com.bibliotrack.controller;
+}
